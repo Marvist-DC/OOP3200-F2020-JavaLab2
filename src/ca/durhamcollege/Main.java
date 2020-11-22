f1fec34cdeab5dcbfe144041ad0f88b2bd0efe76
Tan Vu <vuduytann@gmail.com>
2020-11-22 14:10:37 -0500

    Add comments and finalize the code

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -8,25 +8,35 @@ package ca.durhamcollege;
 
 import java.time.LocalDate;
 
-// The purpose of Main is to display the functioning WorkTicket class with hard coded values
-// to ensure that it is working as intended
+// Main program
 public class Main {
     public static void main(String[] args) {
+        // Try-catch block
         try {
-
+            // Set first work ticket with default parameter
             WorkTicket firstWorkTicket = new WorkTicket();
+
+            // Set second work ticket with parameterized work ticket
             WorkTicket secondWorkTicket = new WorkTicket(1, "abc", LocalDate.of(2000, 12, 12), "something");
+
+            // Print two ticket attributes using overridden toString() method
             System.out.println(firstWorkTicket.toString());
             System.out.println(secondWorkTicket.toString());
 
-            if (firstWorkTicket.setWorkTicket(1, "abc", LocalDate.of(2020, 10, 10), "dhasufh"))
-                System.out.println("\nErrors! No changes to the ticket made.");
+            // setWorkTicket method will return false if any of the input param is invalid
+            if (!firstWorkTicket.setWorkTicket(1, "abc", LocalDate.of(2020, 10, 10), "dhasufh"))
+                System.out.println("\nErrors! No changes to the first ticket.");
+            else
+                System.out.println("\nFirst work ticket are set with new attributes.");
 
-            if (secondWorkTicket.setWorkTicket(1, "abc", LocalDate.of(2020, 10, 10), "dhasufh"))
-                System.out.println("\nErrors! No changes to the ticket made.");
+            if (!secondWorkTicket.setWorkTicket(1, "abc", LocalDate.of(2000, 10, 10), "dhasufh"))
+                System.out.println("\nErrors! No changes to the second ticket.");
+            else
+                System.out.println("\nSecond work ticket are set with new attributes.");
+
         } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch and print any IllegalArgumentException
             System.out.println(illegalArgumentException);
         }
-
     }
 }
